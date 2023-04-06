b23cc06fa77e2b94ee1fcc0fca79b5978b744183
Dylan Phelan <Dtphelan1@gmail.com>
2023-04-06 15:24:59 -0400

    a bit of polish on the institution list ui

--- src/components/InstitutionList/index.tsx
+++ src/components/InstitutionList/index.tsx
@@ -13,31 +13,38 @@ export default function InstitutionList({
     return <></>;
   } else {
     return (
-      <ul className="my-2 max-h-[500px] max-w-lg overflow-auto border">
-        {institutions.map((institution) => (
-          <li
-            key={institution.displayName}
-            className={`flex h-12 items-center border-b border-gray-600 px-2 ${
-              institution.clickable ? "cursor-pointer" : "bg-gray-300"
-            }`}
-            onClick={() => institution.clickable && setInstitution(institution)}
-          >
-            <img
-              src={institution.imgUrl}
-              className="inline h-8 w-8 object-contain"
-              alt={`Logo associated with ${institution.displayName}`}
-            />
-            <div className="ml-2 inline">
-              <h1 className="block font-bold ">{institution.displayName}</h1>
-              {institution.location && (
-                <span className="block text-sm text-gray-600">
-                  {institution.location}
-                </span>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="my-2 max-h-[500px] max-w-lg overflow-auto">
+        <h1 className="mb-2 ">Select an Institution to Export Data From</h1>
+        <ul className="border border-gray-600">
+          {institutions.map((institution) => (
+            <li
+              key={institution.displayName}
+              className={`flex h-12 items-center border-b border-gray-600 px-2 last:border-none hover:bg-blend-darken  ${
+                institution.clickable
+                  ? "cursor-pointer hover:bg-gray-200"
+                  : "bg-gray-300 italic text-gray-600"
+              }`}
+              onClick={() =>
+                institution.clickable && setInstitution(institution)
+              }
+            >
+              <img
+                src={institution.imgUrl}
+                className="inline h-8 w-8 object-contain"
+                alt={`Logo associated with ${institution.displayName}`}
+              />
+              <div className="ml-2 inline">
+                <h1 className="block font-bold ">{institution.displayName}</h1>
+                {institution.location && (
+                  <span className="block text-sm text-gray-600">
+                    {institution.location}
+                  </span>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
