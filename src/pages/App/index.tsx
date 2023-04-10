7acb4f123258011c9fe7486bcb7dc01bcc04ba4d
Dylan Phelan <Dtphelan1@gmail.com>
2023-04-10 08:48:33 -0400

    consolidate separate use-effect hooks

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -18,25 +18,19 @@ export default function App() {
   const [institutions, setInstitutions] = useState<Institution[]>([]);
   const { completeAuthorization } = SMART;
 
-  // Load available institutions on the initial render
   useEffect(() => {
-    getInstitutions().then((institutions: Institution[]) =>
-      setInstitutions(institutions)
-    );
-  }, []);
-
-  // If an institution has been selected, go to the launch page
-  useEffect(() => {
+    // If an institution has been selected, go to the launch page
     if (institution) {
       navigate("/launch");
     }
-  }, [institution, navigate]);
-
-  // On PageLoad, try to authorize
-  useEffect(() => {
+    // Load available institutions on the initial render
+    getInstitutions().then((institutions: Institution[]) =>
+      setInstitutions(institutions)
+    );
+    // On PageLoad, try to authorize
     completeAuthorization();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [institution, navigate]);
 
   return (
     <>
