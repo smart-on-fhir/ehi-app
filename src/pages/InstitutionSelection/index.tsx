6f0f8cb3deff7a226aea6d4b4daea7561a195b7c
Dylan Phelan <Dtphelan1@gmail.com>
2023-04-11 11:09:15 -0400

    remove smartcontext from institution list

--- src/pages/InstitutionSelection/index.tsx
+++ src/pages/InstitutionSelection/index.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useSMARTContext } from "../../context/smartContext";
 import { useInstitutionContext } from "../../context/institutionContext";
 import CodeBlock from "../../components/CodeBlock";
 import InstitutionList from "../../components/InstitutionList";
@@ -9,11 +8,9 @@ import getInstitutions from "../../lib/getInstitutions";
 
 export default function InstitutionSelection() {
   const navigate = useNavigate();
-  const SMART = useSMARTContext();
   const { institution, setInstitution } = useInstitutionContext();
   // List of institutions to be loaded
   const [institutions, setInstitutions] = useState<Institution[]>([]);
-  const { completeAuthorization } = SMART;
 
   useEffect(() => {
     // If an institution has been selected, go to the launch page
@@ -24,8 +21,6 @@ export default function InstitutionSelection() {
     getInstitutions().then((institutions: Institution[]) =>
       setInstitutions(institutions)
     );
-    // On PageLoad, try to authorize
-    completeAuthorization();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [institution, navigate]);
 
@@ -36,7 +31,6 @@ export default function InstitutionSelection() {
         setInstitution={setInstitution}
       />
       <h1>Debugging Purposes</h1>
-      <CodeBlock>{JSON.stringify(SMART, null, 4)}</CodeBlock>
     </>
   );
 }
