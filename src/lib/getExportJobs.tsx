5f362688df82a68edc3979a6caaad843c9a24348
Dylan Phelan <Dtphelan1@gmail.com>
2023-04-12 12:54:36 -0400

    export job list works against ehi-server endpoint

--- src/lib/getExportJobs.tsx
+++ src/lib/getExportJobs.tsx
@@ -1,9 +1,9 @@
 import { ExportJob } from "../types";
 
 export default async function getExportJobs(
-  baseUrl: string,
   signal?: AbortSignal
 ): Promise<ExportJob[]> {
+  const baseUrl = "https://ehi-server.herokuapp.com";
   const exportJobUrl = `${baseUrl}/jobs`;
   const response = await fetch(exportJobUrl, { signal });
   console.log(response);
--- src/pages/ExportJobList/index.tsx
+++ src/pages/ExportJobList/index.tsx
@@ -3,41 +3,31 @@ import Button from "../../components/Button";
 import ExportJobCard from "../../components/ExportJobCard";
 import getExportJobs from "../../lib/getExportJobs";
 import { ExportJob } from "../../types";
-
-function Loading(): JSX.Element {
-  return <h1>Loading Jobs</h1>;
-}
-
-function Error({ error }: { error: Error }): JSX.Element {
-  return (
-    <>
-      <h1>An error occurred</h1>
-      <p>{JSON.stringify(error)}</p>
-    </>
-  );
-}
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 
 export default function ExportJobList() {
-  // const SMART = useSMARTContext();
   // NOTE: For demo purposes, this is a fixed string. In practice, there will be
   //       1...* many EHI servers, one for each institution we request from.
   // QUES: Maybe this should reuse the client but assume one is active?
-  const [url, setUrl] = useState<string>("http://localhost:49632");
   const [jobs, setJobs] = useState<ExportJob[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
   function getJobsUpdateState(signal?: AbortSignal) {
     setLoading(true);
-    getExportJobs(url, signal)
+    getExportJobs(signal)
       .then((response) => {
         console.log(response);
         setError(null);
         setJobs(response);
       })
       .catch((err) => {
-        console.log("An error occurred in job-fetching");
-        setError(err);
+        // If it was a non-abort error, we should track it
+        if (err.name !== "AbortError") {
+          console.log("An error occurred in job-fetching");
+          setError(err);
+        }
       })
       // Always set loading to false when the requests are complete
       .finally(() => {
@@ -58,19 +48,16 @@ export default function ExportJobList() {
   return (
     <>
       <h1>Jobs</h1>
-      <div className="space-x-2">
-        <input
-          type="text"
-          value={url}
-          onChange={(event) => setUrl(event.target.value)}
-        />
-        <Button onClick={() => getJobsUpdateState()}>Refresh Jobs</Button>
-      </div>
       {(() => {
-        if (error) {
-          return <Error error={error} />;
-        } else if (loading) {
-          return <Loading />;
+        if (loading) {
+          return <Loading display={"Loading current export jobs"} />;
+        } else if (error) {
+          return (
+            <ErrorMessage
+              error={error}
+              display="An error occurred in fetching jobs"
+            />
+          );
         } else {
           return (
             <ul className="mt-4">
