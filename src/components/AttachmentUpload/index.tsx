cb6e7290e0918cfbcc3bce06aa92943d50517965
Dylan Phelan <Dtphelan1@gmail.com>
2023-04-26 11:28:36 -0400

    initial handlers for attachment uploading

--- src/components/AttachmentUpload/index.tsx
+++ src/components/AttachmentUpload/index.tsx
@@ -1,6 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
-import { Plus } from "react-feather";
-import Button from "../Button";
+import { Dispatch, SetStateAction, useState } from "react";
 import { LocalAttachment } from "../../types";
 
 type AttachmentUploadProps = {
@@ -10,18 +8,74 @@ type AttachmentUploadProps = {
 export default function AttachmentUpload({
   setAttachment,
 }: AttachmentUploadProps) {
+  const [dragActive, setDragActive] = useState(false);
+  const supportedFiles = [".json", ".csv"];
+
+  // Load files into memory using promises
+  function loadFiles(files: FileList) {
+    console.log(files);
+    setAttachment((prevAttachments: LocalAttachment[]) => [
+      ...prevAttachments,
+      { name: `attachment-${Math.random()}.txt` },
+    ]);
+  }
+
+  function handleDrag(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  }
+
+  function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      loadFiles(e.dataTransfer.files);
+    }
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const filesToAdd = event.target.files;
+    if (filesToAdd && filesToAdd.length > 0) {
+      loadFiles(filesToAdd);
+    }
+  }
   return (
-    <Button
-      className="mb-2 flex w-full justify-center align-baseline"
-      onClick={(e) => {
-        setAttachment((prevAttachments: LocalAttachment[]) => [
-          ...prevAttachments,
-          { name: `attachment-${Math.random()}.txt` },
-        ]);
-      }}
+    <div
+      id="file-upload-zone"
+      className={`my-2 h-16 w-full rounded-lg border-2 border-slate-600 ${
+        dragActive ? "border-solid bg-slate-400" : "border-dashed bg-slate-200"
+      }`}
+      onDragEnter={handleDrag}
+      onDragLeave={handleDrag}
+      onDragOver={handleDrag}
+      onDrop={handleDrop}
     >
-      <Plus className="mr-2" />
-      Add Attachment
-    </Button>
+      <label
+        htmlFor="attachment-input"
+        className="flex h-full cursor-pointer flex-col items-center justify-center"
+      >
+        <p>
+          Drag and drop attachments, or click to{" "}
+          <span className="text-link font-bold">Browse</span>
+        </p>
+        <p className="text-sm italic text-gray-600">
+          Supports {supportedFiles.join(", ")}
+        </p>
+        <input
+          onChange={handleChange}
+          className="hidden"
+          id="attachment-input"
+          type="file"
+          accept={supportedFiles.join(",")}
+          //   multiple
+        />
+      </label>
+    </div>
   );
 }
