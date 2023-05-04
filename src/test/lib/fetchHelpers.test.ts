fb8e01e8d371ab8b737af2173e0e77952a6a51bc
Dylan Phelan <Dtphelan1@gmail.com>
2023-05-04 18:11:12 -0400

    add nock; initial tests for fetch helper

---
+++ src/test/lib/fetchHelpers.test.ts
@@ -0,0 +1,66 @@
+import nock from "nock";
+import { request } from "../../lib/fetchHelpers";
+
+const TEST_URL = "https://example.com";
+
+describe("request", () => {
+  test("Text responses should return as a string", async () => {
+    const response = "response as text";
+    nock(TEST_URL)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/")
+      .reply(200, response);
+    expect(await request(TEST_URL)).toBe(response);
+  });
+  test("JSON Responses should be parsed and returned as an object", async () => {
+    const response = {
+      someKey: "some value",
+    };
+    nock(TEST_URL)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/")
+      .reply(200, response);
+    expect(await request(TEST_URL)).toEqual(response);
+  });
+  test("GET method specified in options should be respected", async () => {
+    const response = "GET successful";
+    nock(TEST_URL)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/")
+      .reply(200, response);
+    expect(await request(TEST_URL, { method: "get" })).toBe(response);
+  });
+  test("POST method specified in options should be respected", async () => {
+    const response = "POST successful";
+    nock(TEST_URL)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .post("/")
+      .reply(200, response);
+    expect(await request(TEST_URL, { method: "post" })).toBe(response);
+  });
+
+  test("Failing requests should throw an error", async () => {
+    const response = "ERROR: Request failed";
+    nock(TEST_URL)
+      .defaultReplyHeaders({
+        "access-control-allow-origin": "*",
+        "access-control-allow-credentials": "true",
+      })
+      .get("/")
+      .replyWithError(response);
+    expect(await request(TEST_URL)).toBe(response);
+  });
+  test("Responses returning an operation outcome should produce a human readable error message", async () => {});
+});
--- test/lib/fetchHelpers.test.ts
+++
@@ -1,8 +0,0 @@
-import { request } from "../../src/lib/fetchHelpers";
-
-describe("request", () => {
-  test("Failing requests should throw an error", () => {});
-  test("JSON Responses should be parsed and returned as an object", () => {});
-  test("Text responses should return as a string", () => {});
-  test("Responses returning an operation outcome should produce a human readable error message", () => {});
-});
