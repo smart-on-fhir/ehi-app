6f5968a7d28b302b69f63dc0440462646fa696a1
Dylan Phelan <Dtphelan1@gmail.com>
2023-05-04 10:13:11 -0400

    add debug helper; refactor user export list

--- src/components/UserExportJobList/index.tsx
+++ src/components/UserExportJobList/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { useAsync } from "../../hooks/useAsync";
 import ExportJobListItemSmall from "../ExportJobListItemSmall";
 import LinkButton from "../../components/LinkButton";
 import { ExportJobSummary } from "../../types";
@@ -9,61 +10,23 @@ import HeadingOne from "../HeadingOne";
 import { Plus } from "react-feather";
 
 export default function UserExportJobList() {
-  const [jobs, setJobs] = useState<ExportJobSummary[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<Error | null>(null);
-
-  function getJobsUpdateState(signal?: AbortSignal) {
-    setLoading(true);
-    getExportJobs(signal)
-      .then((response) => {
-        setError(null);
-        setJobs(response);
-      })
-      .catch((err) => {
-        // If it was a non-abort error, we should track it
-        if (err.name !== "AbortError") {
-          console.log("An error occurred in job-fetching");
-          setError(err);
-        }
-      })
-      // Always set loading to false when the requests are complete
-      .finally(() => {
-        setLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    // Abort controller let's us abort requests on unmount
-    const abortController = new AbortController();
-    const signal = abortController.signal;
-    getJobsUpdateState(signal);
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-  if (loading) {
-    return <Loading display="Loading health record requests" />;
-  } else if (error) {
-    return (
-      <ErrorMessage
-        error={error}
-        display="There was an error loading health record requests"
-      />
-    );
-  } else {
-    return (
-      <>
-        <div className="flex items-baseline justify-between">
-          <HeadingOne>Active EHI Exports</HeadingOne>
-          <LinkButton
-            className="flex items-baseline "
-            to="/institutionSelection"
-          >
-            <Plus size={12} className="mr-2 inline" />
-            New Export
-          </LinkButton>
-        </div>
+  const {
+    loading,
+    result: jobs,
+    error,
+  } = useAsync<ExportJobSummary[]>(useCallback(getExportJobs, []), true);
+  function PageBody() {
+    if (loading) {
+      return <Loading display="Loading health record requests..." />;
+    } else if (error) {
+      return (
+        <ErrorMessage
+          error={error}
+          display="There was an error loading health record requests"
+        />
+      );
+    } else if (jobs) {
+      return (
         <ul className="h-[calc(100vh-210px)] space-y-6 overflow-y-scroll sm:h-[calc(100vh-170px)]">
           {jobs && jobs.length > 0 ? (
             jobs.map((job) => <ExportJobListItemSmall key={job.id} job={job} />)
@@ -71,7 +34,22 @@ export default function UserExportJobList() {
             <p>No jobs were found on the server</p>
           )}
         </ul>
-      </>
-    );
+      );
+    } else {
+      return null;
+    }
   }
+
+  return (
+    <>
+      <div className="flex items-baseline justify-between">
+        <HeadingOne>Active EHI Exports</HeadingOne>
+        <LinkButton className="flex items-baseline " to="/institutionSelection">
+          <Plus size={12} className="mr-2 inline" />
+          New Export
+        </LinkButton>
+      </div>
+      <PageBody />
+    </>
+  );
 }
