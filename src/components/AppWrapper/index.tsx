b8656597bf05aac0bca36996786695838690bb90
Dylan Phelan <Dtphelan1@gmail.com>
2023-05-09 15:19:49 -0400

    fixed page-bottom overlap with wave; fixed h-screen class to be min-h-screen

--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -8,7 +8,7 @@ type AppWrapperProps = {
 
 export default function AppWrapper({ children }: AppWrapperProps) {
   return (
-    <div className="flex h-screen min-w-[100vw] flex-col bg-neutral-100">
+    <div className="flex min-h-screen min-w-[100vw] flex-col bg-neutral-100">
       <div className="text-opacity-900 w-full bg-primary-100 text-stone-600 ">
         <div className="container mx-auto max-w-screen-lg px-4">
           <AppHeader />
@@ -17,10 +17,10 @@ export default function AppWrapper({ children }: AppWrapperProps) {
       <div className="container mx-auto max-w-screen-lg flex-1 flex-shrink-0 px-4 py-8">
         {children}
       </div>
-      <div className="relative w-full bg-primary-700 py-4 pb-8 text-white">
+      <div className="relative mt-8 w-full bg-primary-700 py-4 pb-8 text-white">
         <div
           id="wave-container"
-          className="absolute -top-12 -mx-[calc(-50vw--50%)] h-12 w-full"
+          className="absolute -top-8 -mx-[calc(-50vw--50%)] h-8 w-full"
         >
           <Wave title="Decorative wave" preserveAspectRatio="none" />
         </div>
