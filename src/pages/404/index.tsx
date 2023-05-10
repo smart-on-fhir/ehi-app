2566104daba188f78b2feaff8a5108d3b844f379
Dylan Phelan <Dtphelan1@gmail.com>
2023-05-10 12:44:42 -0400

    better 404 styling

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,4 +1,3 @@
-import { AlertOctagon } from "react-feather";
 import HeadingOne from "../../components/HeadingOne";
 import { useLocation, useNavigate } from "react-router";
 import Button from "../../components/Button";
@@ -9,11 +8,8 @@ export default function FourOhFour() {
   const pageName = location.pathname;
   return (
     <div className="flex flex-col items-center">
-      <HeadingOne>
-        <AlertOctagon className="mr-2 inline" />
-        No Page "{pageName}"
-      </HeadingOne>
-      <p className="mb-4">Sorry, the page you requested was not found.</p>
+      <HeadingOne>No Page "{pageName}"</HeadingOne>
+      <p className="mb-8">Sorry, the page you requested was not found.</p>
       <Button
         variant="emphasized"
         className="mx-auto flex h-12 w-44  items-center justify-center text-xl"
