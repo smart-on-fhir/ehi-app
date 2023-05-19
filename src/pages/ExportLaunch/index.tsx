de601594062e12dec2b33f7432756d2b6d8f436f
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-05-19 13:17:17 -0400

    Render the form immediately

--- src/pages/ExportLaunch/index.tsx
+++ src/pages/ExportLaunch/index.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useSMARTContext } from "../../context/smartContext";
 import { useNavigate } from "react-router";
 import ehiExport from "../../lib/ehiExport";
-import Button from "../../components/Button";
-import LinkButton from "../../components/LinkButton";
+// import Button from "../../components/Button";
+// import LinkButton from "../../components/LinkButton";
 import ErrorMessage from "../../components/ErrorMessage";
 import Loading from "../../components/Loading";
 
@@ -12,7 +12,7 @@ export default function App() {
   const navigate = useNavigate();
   const { client, loading, error } = SMART;
   const [ehiError, setEhiError] = useState<Error | null>(null);
-  const [ehiLink, setEhiLink] = useState<string | null>(null);
+  // const [ehiLink, setEhiLink] = useState<string | null>(null);
 
   // Trigger EHI Export when there is an authorized client
   useEffect(() => {
@@ -27,7 +27,8 @@ export default function App() {
               "redirect",
               window.location.origin + "/jobs"
             );
-            setEhiLink(String(linkWithRedirect));
+            // setEhiLink(String(linkWithRedirect));
+            window.location.assign(linkWithRedirect)
           } else {
             navigate("/jobs");
           }
@@ -61,25 +62,25 @@ export default function App() {
         error={ehiError}
       />
     );
-  } else if (ehiLink) {
-    // Export succeeded, but there is additional user interaction needed
-    return (
-      <div className="mx-auto mt-4 w-full rounded border bg-white p-4">
-        <h1 className="mb-2 text-2xl font-bold">
-          Additional Information Required
-        </h1>
-        <p className="mb-8">
-          In order for the EHI Export request to be processed, there is an
-          additional form to complete.
-        </p>
-        <div className="mt-2 flex justify-between">
-          <LinkButton to="/jobs">Finish Later</LinkButton>
-          <Button autoFocus onClick={() => (window.location.href = ehiLink)}>
-            Complete Form
-          </Button>
-        </div>
-      </div>
-    );
+    // } else if (ehiLink) {
+    //   // Export succeeded, but there is additional user interaction needed
+    //   return (
+    //     <div className="mx-auto mt-4 w-full rounded border bg-white p-4">
+    //       <h1 className="mb-2 text-2xl font-bold">
+    //         Additional Information Required
+    //       </h1>
+    //       <p className="mb-8">
+    //         In order for the EHI Export request to be processed, there is an
+    //         additional form to complete.
+    //       </p>
+    //       <div className="mt-2 flex justify-between">
+    //         <LinkButton to="/jobs">Finish Later</LinkButton>
+    //         <Button autoFocus onClick={() => (window.location.href = ehiLink)}>
+    //           Complete Form
+    //         </Button>
+    //       </div>
+    //     </div>
+    //   );
   } else {
     // Export was successful, there was no link; we should be redirecting to the main page in above effect
     return null;
