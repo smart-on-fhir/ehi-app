28f6ba071b83a6a55953a60771b670b97395667d
Dylan Phelan <Dtphelan1@gmail.com>
2023-05-31 16:26:43 -0400

    big admin-mode banner; appheader looks better on small screens

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -14,7 +14,7 @@ export default function AppHeader() {
         />
         <h1 className="text-center text-2xl ">EHI Export</h1>
       </Link>
-      <nav className="mt-2 flex basis-full space-x-4 sm:mt-0 sm:basis-auto">
+      <nav className="mt-4 flex basis-full space-x-4 sm:mt-0 sm:basis-auto">
         <NavLink
           className={({ isActive }) => (isActive ? "font-bold" : "")}
           to="/"
--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -1,12 +1,15 @@
 import AppFooter from "../AppFooter";
 import AppHeader from "../AppHeader";
 import { ReactComponent as Wave } from "../../assets/wave.svg";
+import useAuthConsumer from "../../context/authContext";
 
 type AppWrapperProps = {
   children?: React.ReactNode;
 };
 
 export default function AppWrapper({ children }: AppWrapperProps) {
+  const { authUser } = useAuthConsumer();
+
   return (
     <div className="flex min-h-screen min-w-[100vw] flex-col bg-neutral-100 accent-active">
       <div className="text-opacity-900 w-full bg-primary-100 text-stone-600 ">
@@ -14,6 +17,11 @@ export default function AppWrapper({ children }: AppWrapperProps) {
           <AppHeader />
         </div>
       </div>
+      {authUser?.role === "admin" && (
+        <div className="w-full bg-active py-2 text-center text-xl text-white">
+          Admin Mode
+        </div>
+      )}
       <div className="container mx-auto max-w-screen-lg flex-1 flex-shrink-0 px-4 py-8">
         {children}
       </div>
