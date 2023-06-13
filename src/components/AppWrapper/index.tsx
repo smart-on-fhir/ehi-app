461cbaea7db5fae5c84419b82eb0732eaa63ebf7
Dylan Phelan <Dtphelan1@gmail.com>
2023-06-13 17:04:23 -0400

    admin banner is red and bold now

--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -18,7 +18,7 @@ export default function AppWrapper({ children }: AppWrapperProps) {
         </div>
       </div>
       {authUser?.role === "admin" && (
-        <div className="w-full bg-active py-2 text-center text-xl text-white">
+        <div className="w-full bg-[#900] py-2 text-center text-xl font-bold text-white">
           Admin Mode
         </div>
       )}
